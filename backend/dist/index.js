004f7e5055718c5ff4a3d56a9c69c185167e0310
kashish00208 <kgupta7827@gmail.com>
2025-06-17 18:44:56 +0530

    Groq chat completion functionality

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -1,8 +1,42 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.main = main;
+exports.getGroqChatCompletion = getGroqChatCompletion;
 const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
-console.log("heyyy");
+console.log("Started working with grok actually");
+const groq_sdk_1 = __importDefault(require("groq-sdk"));
+const groq = new groq_sdk_1.default({ apiKey: process.env.GROQ_API_KEY });
+function main() {
+    return __awaiter(this, void 0, void 0, function* () {
+        var _a, _b;
+        const chatCompletion = yield getGroqChatCompletion();
+        console.log(((_b = (_a = chatCompletion.choices[0]) === null || _a === void 0 ? void 0 : _a.message) === null || _b === void 0 ? void 0 : _b.content) || "");
+    });
+}
+function getGroqChatCompletion() {
+    return __awaiter(this, void 0, void 0, function* () {
+        return groq.chat.completions.create({
+            messages: [
+                {
+                    role: "user",
+                    content: "Write code for TODO application",
+                },
+            ],
+            model: "llama-3.1-8b-instant"
+        });
+    });
+}
+main();
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,3 +1,24 @@
-import dotenv from 'dotenv'
+import dotenv from "dotenv";
 dotenv.config();
-console.log("heyyy")
+console.log("Started working with grok actually");
+
+import Groq from "groq-sdk";
+const groq = new Groq({ apiKey: process.env.GROQ_API_KEY });
+
+export async function main() {
+  const chatCompletion = await getGroqChatCompletion();
+  console.log(chatCompletion.choices[0]?.message?.content || "");
+}
+
+export async function getGroqChatCompletion() {
+  return groq.chat.completions.create({
+    messages: [
+      {
+        role: "user",
+        content: "Write code for TODO application",
+      },
+    ],
+    model : "llama-3.1-8b-instant"
+  });
+}
+main()
